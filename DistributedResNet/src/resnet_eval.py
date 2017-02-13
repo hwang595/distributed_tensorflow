33854e6f51798406b337ee38183fb85e90ea2ae9
hwang595 <hongyiwang@cs.wisc.edu>
2017-02-13 16:05:44 -0600

    and more revisions

--- DistributedResNet/src/resnet_eval.py
+++ DistributedResNet/src/resnet_eval.py
@@ -30,6 +30,7 @@ def main(unused_argv=None):
   if tf.gfile.Exists(FLAGS.eval_dir):
     tf.gfile.DeleteRecursively(FLAGS.eval_dir)
   tf.gfile.MakeDirs(FLAGS.eval_dir)
+  maybe_download_and_extract()
   eval_data, eval_labels = read_validation_data()
   nn_eval.evaluate(eval_data, eval_labels)
 
