089c43b786a726f0920f41e41b356442d5fdc248
hwang595 <hongyiwang@cs.wisc.edu>
2017-03-02 11:37:05 -0600

    move code to somewhere else

--- DistributedResNet/src/distributed_train.py
+++ DistributedResNet/src/distributed_train.py
@@ -299,9 +299,6 @@ def train(target, all_data, all_labels, cluster_spec):
             run_options = tf.RunOptions()
             run_metadata = tf.RunMetadata()
 
-            if FLAGS.timeline_logging:
-                run_options.trace_level=tf.RunOptions.FULL_TRACE
-                run_options.output_partition_graphs=True
             #===============================================================================================    
             if FLAGS.task_id == 0:
                 interval_2 = np.arange(0, int(num_workers))
@@ -336,7 +333,12 @@ def train(target, all_data, all_labels, cluster_spec):
                   tf.logging.info(str(item))
                 tf.logging.info("Solution of LS:")
                 tf.logging.info(str(x))            
-            #=============================================================================================== 
+            #===============================================================================================             
+
+            if FLAGS.timeline_logging:
+                run_options.trace_level=tf.RunOptions.FULL_TRACE
+                run_options.output_partition_graphs=True
+
             tf.logging.info("RUNNING SESSION... %f" % time.time())
             loss_value, step = sess.run(
                 [train_op, global_step], feed_dict=feed_dict, run_metadata=run_metadata, options=run_options)
