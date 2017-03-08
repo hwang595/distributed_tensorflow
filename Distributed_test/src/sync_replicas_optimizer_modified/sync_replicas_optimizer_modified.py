283ac57b4832b717365ea3bfb7eab12c2e5e11da
hwang595 <hongyiwang@cs.wisc.edu>
2017-03-08 12:12:06 -0600

    weighted grad opt test10

--- Distributed_test/src/sync_replicas_optimizer_modified/sync_replicas_optimizer_modified.py
+++ Distributed_test/src/sync_replicas_optimizer_modified/sync_replicas_optimizer_modified.py
@@ -361,10 +361,12 @@ class TimeoutReplicasOptimizer(optimizer.Optimizer):
 
       # Phase 2 gradient applying
       for index, (grad, var) in enumerate(grads_and_vars):
+        ps_print_index = logging_ops.Print(index, [index], message="Index from grad on ps")
+        train_ops.append(ps_print_index)
         with ops.device(var.device):
           grad_accum = self._accumulator_list[index][0]
-          ps_print_test = logging_ops.Print(global_step, [global_step], message="Merge grads from workers happening here!")
-          train_ops.append(ps_print_test)
+          #ps_print_test = logging_ops.Print(global_step, [global_step], message="Merge grads from workers happening here!")
+          #train_ops.append(ps_print_test)
           if grad is None:
             aggregated_grad.append(None)
           elif isinstance(grad, ops.Tensor):
