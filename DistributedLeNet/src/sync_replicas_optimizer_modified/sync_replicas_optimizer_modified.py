6d9cc9ca4be07624cc8ec7b5f4d8f27dbe5d5976
hwang595 <hongyiwang@cs.wisc.edu>
2017-03-21 19:57:56 -0500

    signal test4

--- DistributedLeNet/src/sync_replicas_optimizer_modified/sync_replicas_optimizer_modified.py
+++ DistributedLeNet/src/sync_replicas_optimizer_modified/sync_replicas_optimizer_modified.py
@@ -370,7 +370,7 @@ class TimeoutReplicasOptimizer(optimizer.Optimizer):
               x = self._accumulator_list[0]
               ret = tf.cond(tf.greater_equal(x[0].num_accumulated(), self._constant_for_comparison),
                                          lambda:tf.Tensor(indices=[[0, 0], [1, 2]], values=[1, 2], dense_shape=[3, 4]), 
-                                         lambda:tf.SparseTensor(indices=[[0, 0], [1, 2]], values=[1, 2], dense_shape=[3, 4]))
+                                         lambda:tf.SparseTensor(indices=[[0, 0], [1, 2]], values=[1, 2]))
               if isinstance(ret, ops.Tensor):
                 pos_printer = logging_ops.Print(global_step,
                                                    [global_step],
