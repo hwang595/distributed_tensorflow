05c0b553f2c73f289f83869f981b8a953ae9bf1c
hwang595 <hongyiwang@cs.wisc.edu>
2017-04-16 12:46:59 -0500

    add logging

--- DistributedLeNet/tools/tf_ec2.py
+++ DistributedLeNet/tools/tf_ec2.py
@@ -32,9 +32,9 @@ cfg = Cfg({
     "n_workers" : 1,
     "n_ps" : 1,
     "n_evaluators" : 1,                   # Continually validates the model on the validation data
-    "num_replicas_to_aggregate" : "2",
+    "num_replicas_to_aggregate" : "4",
 
-    "method" : "spot",
+    "method" : "reserved",
 
     # Region speficiation
     "region" : "us-west-2",
@@ -46,7 +46,7 @@ cfg = Cfg({
     "ps_type" : "g2.2xlarge",
     "evaluator_type" : "g2.2xlarge",
 #    "image_id": "ami-2306ba43",
-    "image_id": "ami-35901755",
+    "image_id": "ami-a1198fc1",
 
     # Launch specifications
     "spot_price" : "0.65",                 # Has to be a string
@@ -85,7 +85,7 @@ cfg = Cfg({
     ],
 
     # Model configuration
-    "batch_size" : "256",
+    "batch_size" : "128",
     "max_steps" : "1500",
     "initial_learning_rate" : ".001",
     "learning_rate_decay_factor" : ".95",
